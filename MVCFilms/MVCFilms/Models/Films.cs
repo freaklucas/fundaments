b5855083a972018213a5bb33fa5d8c4e4cf083a9
freaklucas <lucas.oliveira.freak@gmail.com>
2023-05-28 21:56:11 -0300

    post e get

--- MVCFilms/MVCFilms/Models/Films.cs
+++ MVCFilms/MVCFilms/Models/Films.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MVCFilms.Models
 {
@@ -6,10 +7,12 @@ namespace MVCFilms.Models
     {
         public int ID { get; set; }
         public string Title { get; set; } = string.Empty;
+        [Display(Name="Data de lançamento")]
         [DataType(DataType.Date)]
         public DateTime DataLancamento { get; set; }
         public string Description { get; set; } = string.Empty;
         public string Gender { get; set; } = string.Empty;
-        public decimal Price { get; set;}
+        [Column(TypeName = "decimal(18, 2)")]
+        [Display(Name="Value")] public decimal Price { get; set;}
     }
 }
---
+++ apiTest/apiTest/Controllers/CarrController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using static apiTest.Controllers.CarrController;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace apiTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarrController : ControllerBase
+    {
+        private static List<Carr> carrs = new List<Carr>();
+        // GET: api/<CarrController>
+        [HttpGet]
+        public IEnumerable<Carr> Get()
+        {
+            return carrs;
+        }
+
+        // GET api/<CarrController>/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+          //  return "value";
+        //}
+
+        // POST api/<CarrController>
+        [HttpPost]
+        public IActionResult Post([FromBody] Carr objParams)
+        {
+            carrs.Add(objParams);
+            return Ok();
+        } 
+
+        public class Carr
+        {
+            public string Fabric { get; set; }
+            public string Age { get; set; }
+
+        }
+
+        // PUT api/<CarrController>/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        // DELETE api/<CarrController>/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
