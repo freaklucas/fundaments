a501ee52e2fabd3a0661016356a6b95a9345fd01
freaklucas <lucas.oliveira.freak@gmail.com>
2023-06-04 22:01:14 -0300

    new padaria

---
+++ padaria/padaria/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace padaria.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
