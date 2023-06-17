fd749bd7e4793e014af1275f433993584e89df85
freaklucas <lucas.oliveira.freak@gmail.com>
2023-06-17 18:34:09 -0300

    setup contact

--- Pratics/Pratics/Examples.cs
+++ Pratics/Pratics/Examples.cs
@@ -1,7 +1,10 @@
-namespace ExampleNamespace {
+using System;
+
+namespace ExampleNamespace{
   public class Example {
     public static void TestFunction() {
       Console.WriteLine("Testing function");
     }
   }
+
 }
---
+++ contatos/Controllers/ContactController.cs
@@ -0,0 +1,26 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using contatos.Models;
+
+namespace contatos.Controllers;
+
+public class ContactController : Controller
+{
+   public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+    public IActionResult Edit()
+    {
+        return View();
+    }
+    public IActionResult Delete()
+    {
+        return View();
+    }
+}
